de6fdd5ccf03472dfdea324ab871fc9f05b36a99
= <=>
2023-10-19 19:12:47 +0200

    crear nous arxius de conf i modificacio de html

---
+++ Jan/dbConf.php
@@ -0,0 +1,11 @@
+<?php
+// Definir las constantes de conexión a la base de datos
+define("DB_HOST","localhost");
+    define("DB_NAME","users");
+    define("DB_USER","root");
+    define("DB_PSW",'');
+    //DEFINIM PORT
+    define("DB_PORT",3306);
+
+?>
+
